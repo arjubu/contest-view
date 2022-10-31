21dc8accbced55095ab22824ded03d85c243e45c
arjubu <113817394+arjubu@users.noreply.github.com>
2022-10-31 11:51:28 -0500

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home() {
   return (
     <div>
-      <h3> Student Not Presenting Today</h3>
+      <h3> Student Prersentation</h3>
     </div>
   );
 }
